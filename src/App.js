c725f35aeff41e691484981f38111e6396d28cd0
Ankur Seth <iankurseth@gmail.com>
2022-01-15 11:23:59 +0530

    Cleaned src, tailwind classes working OK

--- src/App.js
+++ src/App.js
@@ -5,18 +5,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <p className="px-8 py-20 bg-blue-400 text-2xl text-white">
+          Hello, fs!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
